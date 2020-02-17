aba9c868199a930767b3c49a8f077546994e801d
Bryann Amortegui <brayanbip37@gmail.com>
2020-02-17 02:09:04 -0500

    Api products controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+
+namespace Shop.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller 
+    {
+        
+
+        private readonly IProductRepository productRepository;
+
+        public ProductsController( IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+
+            return Ok(this.productRepository.GetAll());
+        }
+
+
+    }
+}
